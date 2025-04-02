2209e1915012e9c22a04a3b9f79d17add62e15bb
Madeleine Andersson <andersson.madeleine00@gmail.com>
2025-04-02 21:44:20 +0200

    Small chanhes level + finished

--- Assets/Scripts/Madde/Restart Level.cs
+++ Assets/Scripts/Madde/Restart Level.cs
@@ -5,15 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class RestartLevel : MonoBehaviour
 {
+
+    public Transform spawnPosition;
+    private CameraMovement cameraMovement;
+
+    private void Start()
+    {
+        cameraMovement = Camera.main.GetComponent<CameraMovement>();
+
+        if (cameraMovement == null)
+        {
+            Debug.LogError("CameraMovement script not found on Main Camera!");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
 
-
-
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            other.transform.position = spawnPosition.position;
             Debug.Log("Changing scene");
+
+            if (cameraMovement != null)
+            {
+                //cameraMovement.InstantFollow(); // Flytta kameran direkt efter teleportering
+                Debug.Log("Camera moved to new position");
+            }
         }
     }
 }
